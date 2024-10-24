abff701a3af8bab18ad2022866280a8051530a61
heosangbeom <luv_3_u@naver.com>
2024-10-24 16:50:24 +0900

    refactor(step0): 불필요한 문구 삭제
    - 불필요한 문구 삭제

--- src/pages/step/Step2Page.jsx
+++ src/pages/step/Step2Page.jsx
@@ -3,7 +3,6 @@ import Step2Component from "../../components/step/Step2Component.jsx";
 export default function Step2Page() {
     return (
         <div>
-            스텝2 페이지
             <Step2Component/>
         </div>
     );
