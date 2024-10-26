04e9825be4b1bf0cce62dbc858a21516ae1e5f8b
Sung Hyobin <hellmir610@gmail.com>
2024-10-26 14:23:58 +0900

    feat(step2): Step2의 과목명과 저자명, 년도 정보가 실제 데이터를 반영하도록 변경 (#71)
    - step2Api.js
    - Step2Component.jsx

--- src/api/step2Api.js
+++ src/api/step2Api.js
@@ -1,6 +1,10 @@
 import axios from "axios";
 import {BOOK, EXTERNAL, QUESTION_IMAGE} from "./config.js";
 
+export const getBookFromTsherpa = async (bookId) => {
+    return (await axios.get(`${BOOK}/${EXTERNAL}`, {params: {subjectId: bookId}})).data
+}
+
 export const getEvaluationsFromTsherpa = async (bookId) => {
     return (await axios.get(`${BOOK}/${EXTERNAL}/evaluations`, {params: {subjectId: bookId}})).data
 }
