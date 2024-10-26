8cc1fa223232c9aecdeba4afd1efe64d4177f02a
Sung Hyobin <hellmir610@gmail.com>
2024-10-26 23:30:49 +0900

    feat(step2): Step2에서 교재 ID와 지문, 문제 데이터를 전역으로 등록하도록 설정, Step3에서 데이터 수신 (#87)
    - store.jsx
    - examDataSlice.js
    - Step2Component.jsx
    - Step3Component.jsx

--- src/store.jsx
+++ src/store.jsx
@@ -2,11 +2,13 @@ import {configureStore} from "@reduxjs/toolkit";
 import loginSlice from "./slices/loginSlice.jsx";
 import examIdSlice from "./slices/examIdSlice.jsx";
 import bookIdSlice from "./slices/bookIdSlice.jsx";
+import examDataSlice from "./slices/examDataSlice.js";
 
 export default configureStore({
-  reducer: {
-    "loginSlice": loginSlice,
-    "examIdSlice": examIdSlice,
-    "bookIdSlice": bookIdSlice
-  }
+    reducer: {
+        "loginSlice": loginSlice,
+        "examIdSlice": examIdSlice,
+        "bookIdSlice": bookIdSlice,
+        "examDataSlice": examDataSlice
+    }
 })
