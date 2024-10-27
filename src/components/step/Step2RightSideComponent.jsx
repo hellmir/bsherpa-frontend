2ac71f3ceef427eed050486da076c1fd0c99ac52
Sung Hyobin <hellmir610@gmail.com>
2024-10-27 13:34:34 +0900

    feat(step2): Step2의 오른쪽 파트의 햄버거 바 지문 합치기 기능 추가 (#99)

--- src/components/step/Step2RightSideComponent.jsx
+++ src/components/step/Step2RightSideComponent.jsx
@@ -13,6 +13,19 @@ export default function Step2RightSideComponent({itemList}) {
         }
     };
 
+    const groupByPassage = (items) => {
+        return items.reduce((acc, item) => {
+            const passageId = item.passageId || "noPassage";
+            if (!acc[passageId]) {
+                acc[passageId] = [];
+            }
+            acc[passageId].push(item);
+            return acc;
+        }, {});
+    };
+
+    const groupedItems = groupByPassage(itemList);
+
     return (
         <>
             <CommonResource/>
@@ -31,8 +44,8 @@ export default function Step2RightSideComponent({itemList}) {
                 <div className="contents on">
                     <div className="table half-type no-passage">
                         <div className="fix-head">
-                            <span>이동</span>
-                            <span>번호</span>
+                            <span className="move-header">이동</span>
+                            <span className="number-header">번호</span>
                             <span>문제 형태</span>
                             <span>문제 유형</span>
                             <span>난이도</span>
@@ -40,23 +53,34 @@ export default function Step2RightSideComponent({itemList}) {
                         <div className="tbody">
                             <div className="scroll-inner">
                                 <div className="test ui-sortable" id="table-1">
-                                    {itemList.map((item, index) => (
-                                        <div className="col" key={item.itemId}
-                                             onClick={() => handleScrollToQuestion(item.itemId)}>
-                                            <a href="javascript:;">
-                                                <span className="dragHandle ui-sortable-handle ico-move-type01"></span>
-                                                <span className="number-data">{index + 1}</span>
-                                                <span className="tit">
-                <div className="txt">
-                    {truncateText(`${item.largeChapterName} > ${item.mediumChapterName} > ${item.smallChapterName} > ${item.topicChapterName}`, 30)}
-                </div>
-            </span>
-                                                <span
-                                                    className="question-form-data">{item.questionFormCode <= 50 ? "객관식" : "주관식"}</span>
-                                                <span>
-                <span className={`que-badge ${item.difficultyName}`}>{item.difficultyName}</span>
-            </span>
-                                            </a>
+                                    {Object.keys(groupedItems).map((passageId) => (
+                                        <div
+                                            className={`depth-01 ${passageId !== "noPassage" ? "has-passage" : "no-passage"}`}
+                                            key={passageId}>
+                                            {passageId !== "noPassage" && (
+                                                <div className="dragHandle ui-sortable-handle ico-move-type02"></div>
+                                            )}
+                                            <div className="col-group">
+                                                {groupedItems[passageId].map((item, index) => (
+                                                    <div className="col depth-02" key={item.itemId}
+                                                         onClick={() => handleScrollToQuestion(item.itemId)}>
+                                                        <a href="javascript:;">
+                                                            <span
+                                                                className="dragHandle ui-sortable-handle ico-move-type01"></span>
+                                                            <span>{index + 1}</span>
+                                                            <span className="tit">
+                        <div className="txt">
+                            {truncateText(`${item.largeChapterName} > ${item.mediumChapterName} > ${item.smallChapterName} > ${item.topicChapterName}`, 30)}
+                        </div>
+                    </span>
+                                                            <span>{item.questionFormCode <= 50 ? "객관식" : "주관식"}</span>
+                                                            <span>
+                        <span className={`que-badge ${item.difficultyName}`}>{item.difficultyName}</span>
+                    </span>
+                                                        </a>
+                                                    </div>
+                                                ))}
+                                            </div>
                                         </div>
                                     ))}
                                 </div>
