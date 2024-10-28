59d64a08ca1de27da15e228d8d414cc5afaf2c27
5252 <gi1058@naver.com>
2024-10-28 16:03:10 +0900

    pdf 컴포넌트 추가
    - 임시 Step4Page
    - Step4Component
    - StepRouter

--- src/router/stepRouter.jsx
+++ src/router/stepRouter.jsx
@@ -5,10 +5,11 @@ const Step0 = lazy(()=>import("../pages/step/Step0Page.jsx"))
 const Step1 = lazy(()=>import("../pages/step/Step1Page.jsx"))
 const Step2 = lazy(()=>import("../pages/step/Step2Page.jsx"))
 const Step3 = lazy(()=>import("../pages/step/Step3Page.jsx"))
+const Step4 = lazy(()=>import("../pages/step/Step4Page.jsx"))
 const stepRouter = () => {
   return [
     {
-      path:'step0/:bookId',
+      path:'step0',
       element:<Suspense fallback={Loading}><Step0/></Suspense>,
     },
     {
@@ -22,6 +23,10 @@ const stepRouter = () => {
     {
       path:'step3',
       element:<Suspense fallback={Loading}><Step3/></Suspense>,
+    },
+    {
+      path:'step4',
+      element:<Suspense fallback={Loading}><Step4/></Suspense>,
     },
   ]
 }
