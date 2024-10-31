d416275c17e4e094a5f5354dbef9e95d8f266e34
React-test <gitdoolgi@gmail.com>
2024-10-31 17:52:37 +0900

    fix: config.js Question_external 추가

--- src/api/config.js
+++ src/api/config.js
@@ -7,3 +7,9 @@ export const USER = `${API_SERVER_HOST}/users`
 export const BOOK = `${API_SERVER_HOST}/books`
 
 export const QUESTION_IMAGE = `${API_SERVER_HOST}/question-images`
+
+export const EXAM = `${API_SERVER_HOST}/exam`
+
+export const QUESTION_EXTERNAL = `${API_SERVER_HOST}/questions/${EXTERNAL}`
+
+export const ERROR_REPORT = `${API_SERVER_HOST}/error-reports`
