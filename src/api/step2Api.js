5ceed8192a30a5d2f75c5b5d2f7300e8d353468f
whdygks486 <whdygks486@naver.com>
2024-11-05 11:46:48 +0900

    Step2api jwtAxios로 변경

--- src/api/step2Api.js
+++ src/api/step2Api.js
@@ -1,26 +1,28 @@
 import axios from "axios";
+import jwtAxios from "../util/jwtUtil.jsx";
+
 import {BOOK, ERROR_REPORT, EXTERNAL, QUESTION_IMAGE} from "./config.js";
 
 export const getBookFromTsherpa = async (bookId) => {
-    return (await axios.get(`${BOOK}/${EXTERNAL}`, {params: {subjectId: bookId}})).data;
+    return (await jwtAxios.get(`${BOOK}/${EXTERNAL}`, {params: {subjectId: bookId}})).data;
 }
 
 export const getEvaluationsFromTsherpa = async (bookId) => {
-    return (await axios.get(`${BOOK}/${EXTERNAL}/evaluations`, {params: {subjectId: bookId}})).data;
+    return (await jwtAxios.get(`${BOOK}/${EXTERNAL}/evaluations`, {params: {subjectId: bookId}})).data;
 }
 
 export const getAdjustedChapterItemImagesFromTsherpa = async (itemsRequestForm) => {
     const header = {headers: {'Content-Type': 'application/json'}}
 
-    return await axios.post(`${QUESTION_IMAGE}/${EXTERNAL}/chapters/adjust`, itemsRequestForm, header);
+    return await jwtAxios.post(`${QUESTION_IMAGE}/${EXTERNAL}/chapters/adjust`, itemsRequestForm, header);
 }
 
 export const getExamItemImagesFromTsherpa = async (examId) => {
-    return (await axios.get(`${QUESTION_IMAGE}/${EXTERNAL}/exam`, {params: {examId: examId}})).data;
+    return (await jwtAxios.get(`${QUESTION_IMAGE}/${EXTERNAL}/exam`, {params: {examId: examId}})).data;
 }
 
 export const getSimilarItemsImagesFromTsherpa = async (itemId) => {
-    return (await axios.get(`${QUESTION_IMAGE}/${EXTERNAL}/similar-items`, {
+    return (await jwtAxios.get(`${QUESTION_IMAGE}/${EXTERNAL}/similar-items`, {
         params: {
             itemIds: itemId,
             excludedItemIds: 0
@@ -31,5 +33,5 @@ export const getSimilarItemsImagesFromTsherpa = async (itemId) => {
 export const postRegisterErrorReport = async (registerErrorReportForm) => {
     const header = {headers: {'Content-Type': 'multipart/form-data'}}
 
-    return await axios.post(`${ERROR_REPORT}`, registerErrorReportForm, header)
+    return await jwtAxios.post(`${ERROR_REPORT}`, registerErrorReportForm, header)
 }
