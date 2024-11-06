06c1a3cb9d44df0c26a34f84dac7cd63aad71923
React-test <gitdoolgi@gmail.com>
2024-11-06 15:05:47 +0900

    feat: ExamStorageComponent Exam Data 값을 받는 component

---
+++ src/components/common/ExamStorageComponent.jsx
@@ -0,0 +1,58 @@
+import {useSelector} from "react-redux";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import {useQuery} from "@tanstack/react-query";
+import {getExam} from "../../api/mainApi.js";
+import {CircularProgress} from "@mui/material";
+import ExamCardComponent from "./ExamCardComponent.jsx";
+
+
+export default function ExamStorageComponent() {
+    const loginState = useSelector(state=> state.loginSlice)
+    const email = loginState.email;
+
+    const {data: examData, isLoading} = useQuery ({
+        queryKey: ['email', email],
+        queryFn: () => getExam(email),
+        staleTime: 1000 * 3,
+        enabled: !!email
+    });
+
+    const exams = examData?.getExamResponses || [];
+    console.log("examData: ", examData);
+
+    return (
+        <Box component="main" sx={{ flexGrow: 1, bgcolor: 'background.default', p: 3 }}>
+            <Toolbar />
+
+            {isLoading ? (
+                <Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', minHeight: '200px' }}>
+                    <CircularProgress />
+                </Box>
+            ) : (
+                // 로딩이 끝나고 데이터가 있을 때 표시
+                exams.length > 0 ? (
+                    <Box sx={{ marginBottom: 2, marginTop: 2 }}>
+                        <Typography variant="h6">{exams[0].username}님의 시험지 보관함</Typography>
+                        {exams.map((exam, index) => (
+                            <Box key={exam.id} sx={{ display: 'flex', gap: 2, marginBottom: 2, marginTop: 2 }}>
+                                {/* 여기에 ExamCardComponent를 추가하여 렌더링 */}
+                                <ExamCardComponent
+                                    key={exam.id}
+                                    examName={exam.examName}
+                                    className={exam.className}
+                                    subjectName={exam.subjectName}
+                                    grade={exam.grade}
+                                />
+                            </Box>
+                        ))}
+                    </Box>
+                ) : (
+                    // 데이터가 없을 때 표시
+                    <Typography variant="body1">{examData[0]?.username}님이 작성한 시험지가 없습니다.</Typography>
+                )
+            )}
+        </Box>
+    );
+}
