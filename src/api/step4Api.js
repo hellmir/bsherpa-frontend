cef6739cbfdc70011828ce46ba9d8797fa407e61
5252 <gi1058@naver.com>
2024-11-06 17:01:51 +0900

    PDF 출력 양식 수정
    - 실제 데이터 맞춤 양식으로 수정 중
    - 현재 examHeader 틀만 맞춤

--- src/api/step4Api.js
+++ src/api/step4Api.js
@@ -4,10 +4,9 @@ import axios from "axios";
 
 export const getExamTest = async () =>{
     try{
-        console.log("getExamTest()");
+        console.log("getExamTest() 호출됨");
         const header = {headers: {'Content-Type': 'application/json'}};
         const response = await jwtAxios.get(`${API_SERVER_HOST}/exams/31`, header);
-        console.log("API: ", response);
         return response.data;
 
         //return (await axios.get(`${API_SERVER_HOST}/exam`, header)).data;
