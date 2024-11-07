691936630946ff35ff87a1d48dcec80ecca29d94
whdygks486 <whdygks486@naver.com>
2024-11-07 11:04:16 +0900

    재 업로드 시험지 보관함 해당 과목에대한 데이터 조회와 해당하는 과목에 대한 교과서 이미지 추가

--- src/components/common/ExamCardComponent.jsx
+++ src/components/common/ExamCardComponent.jsx
@@ -19,7 +19,6 @@ import Step4Component from "../step/Step4Component.jsx";
 export default function ExamCardComponent({examId, subjectName, examName, grade}) {
     const {moveToStepWithData} = useCustomMove();
 
-    // 과목별 이미지 매핑 함수
     const getSubjectImage = (subject) => {
         switch (subject) {
             case '국어':
@@ -43,21 +42,12 @@ export default function ExamCardComponent({examId, subjectName, examName, grade}
 
     const handleClickLoadExamPage = () => {
         window.open("https://exsherpa.com", "_blank");
-    }
+    };
 
     return (
-        <Card sx={{ 
-            maxWidth: 345,
-            height: '100%',
-            display: 'flex',
-            flexDirection: 'column'
-        }}>
+        <Card sx={{ maxWidth: 345, height: '100%', display: 'flex', flexDirection: 'column' }}>
             <CardMedia
-                sx={{ 
-                    height: 140,
-                    backgroundSize: 'cover',
-                    backgroundPosition: 'center'
-                }}
+                sx={{ height: 140, backgroundSize: 'cover', backgroundPosition: 'center' }}
                 image={getSubjectImage(subjectName)}
                 title={`${subjectName} 교과서 이미지`}
             />
@@ -69,13 +59,8 @@ export default function ExamCardComponent({examId, subjectName, examName, grade}
                     {subjectName} {grade}
                 </Typography>
             </CardContent>
-            <CardActions sx={{ 
-                marginTop: 'auto',
-                display: 'flex',
-                justifyContent: 'space-between',
-                padding: '8px 16px'
-            }}>
-                <Step4Component examId={examId}/>
+            <CardActions sx={{ marginTop: 'auto', display: 'flex', justifyContent: 'space-between', padding: '8px 16px' }}>
+                <Step4Component examId={examId} />
                 <Button size="small" onClick={handleClickLoadExamPage}>
                     온라인 시험지 보기
                 </Button>
