23fe5b05779db1598bdb0d254f93bcd161f9a43e
gitdoolgi <gitdoolgi@gmail.com>
2024-11-10 17:08:37 +0900

    feat: Step3SuccessComponent save image import, src 변경

--- src/components/step/Step3SuccessComponent.jsx
+++ src/components/step/Step3SuccessComponent.jsx
@@ -1,5 +1,6 @@
 import CommonResource from "../../util/CommonResource.jsx";
 import useCustomMove from "../../hooks/useCustomMove.jsx";
+import Save from '../../assets/save.png';
 
 export default function Step3SuccessComponent(){
     const {moveToPath}= useCustomMove();
@@ -23,7 +24,7 @@ export default function Step3SuccessComponent(){
                     </div>
 
                     <div className="save-wrap">
-                        <img src="../src/assets/images/save.png" width="10%" height="auto" alt="저장완료 이미지" />
+                        <img src={Save} width="10%" height="auto" alt="저장완료 이미지" />
                         <span className="txt">
                             시험지가 저장되었습니다.
                             <p>저장된 시험지는 시험지 보관함 메뉴에서 확인 가능합니다. </p>
