e9d58228c248c0ffe51126ece8bcea8e9e040b50
Sung Hyobin <hellmir610@gmail.com>
2024-11-15 15:20:06 +0900

    refactor(step2): Step2를 타입스크립트로 리팩터링 (#365)
    - ConfirmationModal.tsx

--- src/components/common/ConfirmationModal.tsx
+++ src/components/common/ConfirmationModal.tsx
@@ -1,7 +1,26 @@
 import React from "react";
-import '../../assets/css/confirmationModal.css';
+import "../../assets/css/confirmationModal.css";
 
-export default function ConfirmationModal({title, message, details, onCancel, onConfirm}) {
+interface Detail {
+    level: string;
+    count: number;
+}
+
+interface ConfirmationModalProps {
+    title: string;
+    message: string;
+    details: Detail[];
+    onCancel: () => void;
+    onConfirm: () => void;
+}
+
+const ConfirmationModal: React.FC<ConfirmationModalProps> = ({
+                                                                 title,
+                                                                 message,
+                                                                 details,
+                                                                 onCancel,
+                                                                 onConfirm,
+                                                             }) => {
     const total = details.reduce((sum, detail) => sum + detail.count, 0);
 
     return (
@@ -25,10 +44,16 @@ export default function ConfirmationModal({title, message, details, onCancel, on
                 </div>
                 <p>해당 문제 구성으로 재검색하시겠습니까?</p>
                 <div className="modal-buttons">
-                    <button className="cancel-button" onClick={onCancel}>취소</button>
-                    <button className="confirm-button" onClick={onConfirm}>확인</button>
+                    <button className="cancel-button" onClick={onCancel}>
+                        취소
+                    </button>
+                    <button className="confirm-button" onClick={onConfirm}>
+                        확인
+                    </button>
                 </div>
             </div>
         </div>
     );
-}
+};
+
+export default ConfirmationModal;
