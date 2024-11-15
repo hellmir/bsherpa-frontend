511594d08bdc9d43ebefe8c48b4055ab7aea5701
Sung Hyobin <hellmir610@gmail.com>
2024-11-15 11:31:25 +0900

    fix(step2): 누락된 삭제 파일들 추가 (#365)

--- src/pages/step/Step2Page.jsx
+++
@@ -1,9 +0,0 @@
-import Step2Component from "../../components/step/Step2Component.jsx";
-
-export default function Step2Page() {
-    return (
-        <div>
-            <Step2Component/>
-        </div>
-    );
-}
